9f18c2c98b81a700527350ddaa8a5b717d062cd3
catbea <674362263@qq.com>
2017-05-19 17:59:22 +0800

    没啥

--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -1,5 +1,6 @@
 <template lang="html">
-  <div class="ratings">
+  <div ref="ratingWrapper" class="ratings">
+    <div class="ratingWrapper">
       <div class="overview">
         <div class="overview-left">
           <h1 class="score">{{seller.score}}</h1>
@@ -29,10 +30,35 @@
       </div>
       <split></split>
       <ratingselect :ratings="ratings" :selectType="selectType" :onlyContent="onlyContent"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar" alt="">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                  <star :size="24" :score="rating.score"></star>
+                  <span class="delivery"></span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend||rating.recommend.length>0">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from "better-scroll";
+import {formatDate} from "../../common/js/date.js";
 import star from "../star/star.vue";
 import split from "../split/split.vue";
 import ratingselect from "../ratings/ratingselect.vue";
@@ -51,11 +77,23 @@ export default {
       onlyContent:true
     };
   },
+  filters:{
+      formatDate(time){
+        let data=new Date(time);
+        return formatDate(data,'yyyy-MM-dd hh:mm');
+
+      }
+  },
   created(){
     this.$http.get('/api/ratings').then((response)=>{
       response=response.body;
       if(response.errno===ERR_OK){
         this.ratings=response.data;
+        this.$nextTick(()=>{
+          this.menuScroll=new BScroll(this.$refs.ratingWrapper,{
+            click:true
+          });
+        });
       }
     });
   },
